2e881504258ef687a03637ea1f2a30f0c1bd4731
pozdnyshevmax <max_rash@mail.ru>
2015-11-03 13:48:53 +0600

    start

---
+++ CRC32_calc/CRC32_calc/CRC32_calc.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> tags;
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		cout << "ERROR: need filename or mask" << endl;
+	}
+	
+	system("pause");
+	return 0;
+}
