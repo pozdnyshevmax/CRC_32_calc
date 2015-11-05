f06d110c1e0b449bb0b53592f0350e051166b3ab
pozdnyshevmax <max_rash@mail.ru>
2015-11-05 11:20:05 +0600

    commenting

--- CRC32_calc/CRC32_calc/CRC32_calc.cpp
+++ CRC32_calc/CRC32_calc/CRC32_calc.cpp
@@ -11,35 +11,34 @@
 typedef long long count_wc;
 using namespace std;
 
-string int_to_hex(int i) {
+string int_to_hex(int i) { //hex output of int
 	std::ostringstream oss;
 	oss << "0x" << hex << i;
 	return oss.str();
 }
 
-string calc_tag(string file, string BeginTag,string EndTag) {
-	string search = "<" + BeginTag + ">[^'<',(0x)]*<" + EndTag + ">";
+string calc_tag(string file, string BeginTag,string EndTag) { //calcuate text by template <BeginTag>TEXT<EndTag>
+	string search = "<" + BeginTag + ">[^'<',(0x)]*<" + EndTag + ">"; // search string
 	std::regex rx(search);
 	std::smatch m;
-	while (std::regex_search(file, m, rx)) {
+	while (std::regex_search(file, m, rx)) { //while still find by expression
 		for (auto x : m) {
-			string in = x.str().erase(0, BeginTag.size() + 2);
-			in.erase(in.end() - (EndTag.size() + 3),in.end());
+			string in = x.str().erase(0, BeginTag.size() + 2); //
+			in.erase(in.end() - (EndTag.size() + 3),in.end()); // delete tags
 			crc_32 code;
-			code.ProcessCRC((void *)in.c_str(), in.size());
-			string replacement = "<" + BeginTag + ">" +int_to_hex(code.m_crc32) + "<" + EndTag + ">";
+			code.ProcessCRC((void *)in.c_str(), in.size()); // calculate CRC_32 summ
+			string replacement = "<" + BeginTag + ">" +int_to_hex(code.m_crc32) + "<" + EndTag + ">"; // prepare replace string
 			file = regex_replace(file, rx, replacement, regex_constants::format_first_only);
 		}
 	}
 	return file;
 }
 
-string calc_declaration(string file) {
+string calc_declaration(string file) { //calcuate text by template DECLARATION(TEXT)
 	for (long long i = 1; i < file.size(); i++) {
 		if (file[i] == '(' && (isalpha(file[i - 1]) || ('0' <= file[i - 1] && '9' >= file[i - 1]))) {
 			int end = file.find(')', i);
 			string text = file.substr(i + 1, end - i - 1);
-			//cout << text << endl;
 			crc_32 code;
 			code.ProcessCRC((void *)text.c_str(), text.size());
 			text = int_to_hex(code.m_crc32);
@@ -60,13 +59,16 @@ void calc_file(char *filename,int tag_count, char **tags) {
 		pbuf->sgetn(buffer, Length);
 		buffer[Length] = '\0';
 		string file = string(buffer);
-		for (int i = 2; i < tag_count; i += 2)
+
+		for (int i = 2; i < tag_count; i += 2) //for all tags
 			file = calc_tag(file, string(tags[i]), string(tags[i + 1]));
 		file = calc_declaration(file);
+
 		name = "result_" + name;
-		ofstream ofs(name.c_str());
+		ofstream ofs(name.c_str()); // put result in file with prefix "result_"
 		ofs << file;
 		ofs.close();
+
 		delete[] buffer;
 	}
 	ifs.close();
@@ -84,7 +86,7 @@ int main(int argc, char *argv[])
 	}
 	WIN32_FIND_DATAA FindFileData;
 	HANDLE hFind;
-	hFind = FindFirstFileA(argv[1], &FindFileData);
+	hFind = FindFirstFileA(argv[1], &FindFileData); // take a first file in maindir by mask
 	if (hFind == INVALID_HANDLE_VALUE)
 	{
 		printf("FindFirstFile failed (%d)\n", GetLastError());
@@ -94,9 +96,9 @@ int main(int argc, char *argv[])
 	else
 	{
 		do {
-			if (!string(FindFileData.cFileName).find("result_")) continue;
-			calc_file(FindFileData.cFileName,argc,argv);
-		} while (FindNextFileA(hFind, &FindFileData) != 0);
+			if (!string(FindFileData.cFileName).find("result_")) continue; //exclude "result_*" files
+			calc_file(FindFileData.cFileName,argc,argv); // calculate file
+		} while (FindNextFileA(hFind, &FindFileData) != 0); //while still find by mask
 
 		FindClose(hFind);
 	}
